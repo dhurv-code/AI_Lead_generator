256e3d11d4778c666e8db6457fed26a92a196026
Dhurv Gupta <dhurvgupta143@gmail.com>
2026-06-08 23:39:31 +0530

    updatetion

---
+++ backend/app/database/lead_respository.py
@@ -0,0 +1,6 @@
+from app.database.mongodb import leads_collection
+def save_lead(document):
+    return lead_collection.insert_one(document)
+
+def get_all_leads():
+    return list(leads_collection.find())
