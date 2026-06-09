729d25492dad3d21cf85226dc58d31734e698856
Dhurv Gupta <dhurvgupta143@gmail.com>
2026-06-09 14:36:24 +0530

    updation

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 from app.schemas.request_schema import AnalyzeRequest
 from app.services.scraper import scrape_website
 from app.services.analyzer import analyze_content
+from app.services.email_generator import generate_email
+
 
 router = APIRouter()
 
@@ -15,9 +17,14 @@ def health_check():
 def analyze_website(request: AnalyzeRequest):
 
     website_data = scrape_website(request.url)
+    audit = analyze_content(website_data["content"])
 
-    audit = analyze_content(
-        website_data["content"]
+    email=generate_email(
+        audit["business_type"],
+        audit["issues"],
+        audit["recommendation"]
     )
+
+    audit["email"]=email
 
     return audit
---
+++ backend/app/services/batch_processor.py
@@ -0,0 +1,4 @@
+import pandas as pd
+def process_csv(file_path):
+    df=pd.read_csv(file_path)
+    return df["website"].tolist()
